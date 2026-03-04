1af0b27bb3d4cf882b7d18a745dbad374ff60547
Royahp <Roya89.hosseinpour@gmail.com>
2026-03-04 20:31:30 +0100

    sort added

--- server/index.mjs
+++ server/index.mjs
@@ -38,13 +38,16 @@ function Question (text,userId,date) {
             }
         }return afterDateAnswers;
     }
+    this.listScore = () => {
+        return [...this.answers].sort((a,b) => b.score - a.score)
+    }
 }
 
 const q1 = new Question("how long is this exercise?",1,"2026-02-27")
 
-const a1 = new Answer ("too much",2,"2026-04-11")
-const a2 = new Answer ("short",2,"2026-04-05")
-const a3 = new Answer ("10 min",3,"2026-02-26")
+const a1 = new Answer ("too much",2,"2026-04-11",10)
+const a2 = new Answer ("short",2,"2026-04-05",5)
+const a3 = new Answer ("10 min",3,"2026-02-26",40)
 
 q1.addAnswer(a1);
 q1.addAnswer(a2);
@@ -55,4 +58,6 @@ const user2 = q1.getAnswer(2);
 //console.log(user2);
 const yesterday = dayjs ("2026-01-01")
 const dateAfterYesterday = q1.afterDate (yesterday)
-console.log (dateAfterYesterday)
+//console.log (dateAfterYesterday)
+const ListedByScore = q1.listScore ()
+console.log(ListedByScore)
