15504389544cf83a6ce84532dc5cf017cc16a473
Royahp <Roya89.hosseinpour@gmail.com>
2026-03-21 15:00:45 +0100

    getfav

--- labs/lab5/server/FilmLibrary.mjs
+++ labs/lab5/server/FilmLibrary.mjs
@@ -24,5 +24,27 @@ function getAllFilms(){
     })
 }
 
+function getFavorite (){
+    return new Promise((resolve,reject)=>{
+        const sql = "SELECT * FROM films WHERE favorite = ?"
+        const myfav = 1;
+        db.all(sql,[myfav],(err,rows)=>{
+            if (err) {
+                reject (err)
+                return
+            }
+        const favFilms =rows.map (row => new Film(
+            row.id ,
+            row.title ,
+            Boolean(row. favorite) ,
+            row.watchDate ,
+            row.rating ,
+            row.userId
+        ))
+        resolve (favFilms)
+        })
 
-export {getAllFilms}
+    })
+}
+
+export {getAllFilms,getFavorite}
--- labs/lab5/server/server.mjs
+++ labs/lab5/server/server.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
 import sqlite3 from 'sqlite3';
-import { getAllFilms } from './FilmLibrary.mjs';
+import { getAllFilms ,getFavorite} from './FilmLibrary.mjs';
 
 const app = express();
 const PORT = 3001;
@@ -15,4 +15,10 @@ app.get ('/api/films',(req,res) =>{
     getAllFilms()
        .then((films)=>res.json(films))
        .catch(err => res.status(500).json(err))
+})
+
+app.get('/api/fav' ,(req,res) =>{
+    getFavorite()
+        .then((favilms)=>res.json(favilms))
+        .catch(err=>res.status(500).json(err))
 })
