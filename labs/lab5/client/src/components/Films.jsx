1ca337379665ba66335f42cf06e37826abb0a6c5
Royahp <Roya89.hosseinpour@gmail.com>
2026-03-23 14:40:04 +0100

    message

--- labs/lab5/client/src/components/Films.jsx
+++ labs/lab5/client/src/components/Films.jsx
@@ -1,14 +1,7 @@
 import { ListGroup } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 function FilmList(props) {
-  const { films } = props;
-
-  return (
-    <ListGroup>
-      {films.map(film => (
-        <FilmRow film={film} key={film.id} />
-      ))}
-    </ListGroup>
-  );
+  
+  
 }
 export default FilmList
