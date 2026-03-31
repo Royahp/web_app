0901629c6e8930eca4b10a137b79644bf07dac6e
Royahp <Roya89.hosseinpour@gmail.com>
2026-03-31 17:02:19 +0200

    lab7 built

---
+++ labs/lab7/my-app/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Container, Col, Row } from "react-bootstrap";
+import React from "react";
+function Header(props) {
+  return (
+    <Container fluid className="text-center mt-3">
+      <Row>
+        <Col md={4}>Library</Col>
+        <Col md={4}>
+          <input placeholder="search..."></input>
+        </Col>
+        <col md={4}>log in</col>
+      </Row>
+    </Container>
+  );
+}
+export default Header;
